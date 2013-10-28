1bbce4fb01e49e7d89c9f18cc3c1b7a27948e335
Jake Siegel <jxs1073@rit.edu>
2013-10-28 14:53:46 -0400

    may have accidentally added preferences. if possible, do selective merge when pulling this commit

--- src/com/example/workoutcompanion/controller/Receiver.java
+++ src/com/example/workoutcompanion/controller/Receiver.java
@@ -2,4 +2,24 @@ package com.example.workoutcompanion.controller;
 
 public class Receiver {
 
+	public boolean CreateWorkout() {
+		
+	}
+	
+	public boolean EditWorkout() {
+		
+	}
+	
+	public boolean CreateExercise() {
+		
+	}
+	
+	public boolean EditExercise() {
+		
+	}
+	
+	public boolean EditProfile() {
+		
+	}
+	
 }
