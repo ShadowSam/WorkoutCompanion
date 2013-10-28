8ff77092a1030c8f21abb2ee832e62ea89b882f4
Samantha Shandrow <samantha.shandrow@gmail.com>
2013-10-28 14:49:53 -0400

    First attempt at the interface Controller.

---
+++ src/com/example/workoutcompanion/controller/Command.java
@@ -0,0 +1,12 @@
+package com.example.workoutcompanion.controller;
+
+/**
+ *  Command interface
+ */
+
+public interface Command 
+{
+	public Boolean Create();
+	public Boolean Edit();
+	public Boolean Delete();
+}
