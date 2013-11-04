d156839b43eddd99ea978a9dddd0f89e4f6e0eb6
Jake Siegel <jxs1073@rit.edu>
2013-11-04 14:21:42 -0500

    small receiver changes

--- src/com/example/workoutcompanion/controller/Receiver.java
+++ src/com/example/workoutcompanion/controller/Receiver.java
@@ -1,11 +1,16 @@
 package com.example.workoutcompanion.controller;
 
+import android.content.Context;
 import com.example.workoutcompanion.db.*;
 import com.example.workoutcompanion.dom.*;
 
 public class Receiver {
 
-	private DatabaseHandler DBH = new DatabaseHandler(null);
+	private DatabaseHandler DBH;
+	
+	public Receiver(Context context) {
+		DBH = new DatabaseHandler(context);
+	}
 	
 	public boolean CreateWorkout(String workoutName) {
 		Workout newWorkout = new Workout(workoutName);
