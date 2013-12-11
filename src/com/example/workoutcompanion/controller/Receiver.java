7b3ca52e66c721c56d25d8156b44b171b2dd0885
Jake Siegel <jxs1073@rit.edu>
2013-12-11 14:25:09 -0500

    finished receiver/db integration or whatever, for what functions we have completed

--- src/com/example/workoutcompanion/controller/Receiver.java
+++ src/com/example/workoutcompanion/controller/Receiver.java
@@ -16,22 +16,44 @@ public class Receiver {
 		DBH = new DatabaseHandler(context);
 	}
 	
+	/**
+	 * The handler for the database.
+	 */
 	private DatabaseHandler DBH;
 
+	/**
+	 * Helper function to build workouts so that code is not replicated.
+	 * @param wN Name of the workout to build
+	 * @return Workout object
+	 */
 	private Workout buildWorkout(String wN) {
 		return new Workout(wN);
 	}
+	
+	/**
+	 * Helper function to build exercises so that code is not replicated.
+	 * @param eN Name of the exercise to build
+	 * @return Exercise object
+	 */
 	private Exercise buildExercise(String eN) {
 		return new Exercise(eN);
 	}
 
 	/**
 	 * Creates workout for given string name and array of string exercises.
-	 * @param workoutName String Name of workout
-	 * @param exercises String[] Array of exercise names
+	 * @param workoutName Name of workout
+	 * @param exercises Array of exercise names
 	 * @return boolean success
 	 */
 	public boolean CreateWorkout(String workoutName,String[] exercises) {
+		/*
+		 * Function to create a workout and enter it into the database.
+		 * If a workout is successfully created, then the function will return
+		 * TRUE. If there is an error with the connection to the database, then
+		 * a stack trace will be printed and the function will return FALSE.
+		 * There is no other situation where FALSE will be returned -- even if
+		 * a workout is already in the database, it can still be added.
+		 */
 		Workout workout = buildWorkout(workoutName);
 		try {
 			for(String exerciseName: exercises) {
@@ -42,19 +64,21 @@ public class Receiver {
 			e1.printStackTrace();
 			return false;
 		}
-		
 		return true;
 	}
 
 	/**
 	 * Edits the database entry for given workout to hold new exercises
-	 * @param workoutName String Name of workout to edit
-	 * @param exToAdd String[] Names of exercises to add to workout
-	 * @param exToRem String[] Names of exercises to remove from workout
+	 * @param workoutName Name of workout to edit
+	 * @param exToAdd Array of exercise names to add to workout
+	 * @param exToRem Array of exercise names to remove from workout
 	 * @return boolean success
 	 */
 	public boolean EditWorkout(String workoutName,
 			String[] exToAdd, String[] exToRem) {
+		/*
+		 * 
+		 */
 		Workout workout;
 		try {
 			workout = DBH.findWorkout(workoutName);
@@ -69,7 +93,7 @@ public class Receiver {
 				// Attempt to find exercise in DB, then add exercise to workout
 				Exercise exercise;
 				if ((exercise = DBH.findExercise(e)) == null)
-					return false; // doesn't exist
+					return false; // doesn't exist; create exercise?
 				workout.getExercises().add(exercise);
 				exercise.getWorkouts().add(workout);
 			} catch (SQLException e1) {
@@ -81,17 +105,21 @@ public class Receiver {
 		
 		for (String e : exToRem) {
 			try {
-				workout.getExercises().remove(DBH.findExercise(e));
-				// It is possible to remove something from the list if it is not
-				// even in the list; this is not a problem, generally.
-				// Also a necessity: Removing the workout from the exercise
+				Exercise exercise;
+				if ((exercise = DBH.findExercise(e)) == null)
+					return false; // exercise doesn't exist
+				workout.getExercises().remove(exercise);
+				exercise.getWorkouts().remove(workout);
+				/* 
+				 * It is possible to remove something from the list if it is not
+				 * even in the list; this is not a problem, generally.
+				 */
 			} catch (SQLException e1) {
 				// The exercise does not exist in the database
 				e1.printStackTrace();
 				return false; // do this for now
 			}
-			// Remove exercise from workout
-		} // Removing exercises from workouts; exercises exist in DB
+		}
 		
 		try {
 			if (DBH.addOrUpdateWorkout(workout) == null)
@@ -114,7 +142,11 @@ public class Receiver {
 		// If exercise exists in db, return false :)
 		try {
 			if (DBH.findExercise(exerciseName) != null)
-				return false; // exercise present in db already
+				return false;
+			/*
+			 * The database already contains an entry for an exercise of this name.
+			 * You shouldn't be creating duplicate entries, so we want this to fail.
+			 */
 		} catch (SQLException e) {
 			// Database failure
 			e.printStackTrace();
@@ -143,12 +175,20 @@ public class Receiver {
 	 */
 	public boolean EditExercise(String exerciseName) {
 		// The functionality of this depends on what we want to be editable.
+		// Obviously incomplete.
 		try {
 			if (DBH.addOrUpdateExercise(buildExercise(exerciseName)) == null)
 				return false;
+			/*
+			 * in theory, this should never be null, since an exercise run through
+			 * this function will be added to the database if it does not exist
+			 * to be updated. however, it is appropriate to consider so-called
+			 * "impossible" scenarios.
+			 */
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			// SQL DB failure
 			e.printStackTrace();
+			return false;
 		}
 		return true;
 	}
